2069c2845871f74072c331b4e942af9af02244dd
LakkappaBJ <lucky.xworkz@gmail.com>
2021-12-27 23:01:44 +0530

    to change Nandihill

---
+++ Hills.java
@@ -0,0 +1,19 @@
+class Hills{
+		public static void main(String []args){ 
+			System.out.println("main() started");
+			int num=0;
+			for(int i=0;i<args.length;i++){
+				if(num==8){
+				System.out.println();
+				num = 0;
+				
+				}
+		args[1] = "NandhiHill";
+		System.out.print(args[i]+"\t");
+		num = num+1;
+
+		}
+
+	}
+}
+	
